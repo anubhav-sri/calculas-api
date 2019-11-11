815ea8fd4e00f79e3d69e26290847c977fcd4c71
Anubhav Srivastava <anubhavsrivastava@AnubhavtavasMBP.workgroup>
2019-11-12 00:55:54 +0100

    Addidng custom error response

--- src/main/java/com/anubhav/calculas/controllers/CalculasExceptionHandler.java
+++ src/main/java/com/anubhav/calculas/controllers/CalculasExceptionHandler.java
@@ -1,17 +1,17 @@
 package com.anubhav.calculas.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class CalculasExceptionHandler {
+public class CalculasExceptionHandler extends ResponseEntityExceptionHandler {
+
 
     @ExceptionHandler(value = InvalidExpressionException.class)
-    public void handleInvalidExpressionException(HttpServletResponse response) throws IOException {
-       response.sendError(HttpStatus.BAD_REQUEST.value());
+    public ResponseEntity<ErrorResponse> handleInvalidExpressionException(InvalidExpressionException ex) {
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/anubhav/calculas/controllers/ErrorResponse.java
+++ src/main/java/com/anubhav/calculas/controllers/ErrorResponse.java
@@ -8,4 +8,12 @@ public class ErrorResponse {
     public ErrorResponse(String message) {
         this.message = message;
     }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public boolean isError() {
+        return error;
+    }
 }
